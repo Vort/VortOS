056dbcaaa3587f52125435cff967f61c4876665e
Vort <vvort@yandex.ru>
2012-03-10 11:04:03 +0200

    improve build

--- DriverPack/Compile.bat
+++ DriverPack/Compile.bat
@@ -1,11 +1,11 @@
-del *.bi_
-del *.bin
+set outdir=Release
+mkdir %outdir%
+del %outdir%\* /Q
 
-del *.obj
+set linkparams=-nologo -nod -fixed -opt:ref -entry:Entry -subsystem:windows ..\my_memcpy.obj ..\my_memset.obj OpNewDel.obj String2.obj
 
-set linkparams=-nologo -nod -fixed -opt:ref -entry:Entry -subsystem:windows my_memcpy.ob_ my_memset.ob_ OpNewDel.obj String2.obj
-
-cl.exe -c -GR- -GS- -Gs999999999 -Gy -Ox -Oy -O2 -Oi- -fp:fast Hello.cpp Loader.cpp Test.cpp i8042.cpp PS2Keyb.cpp PS2Mouse.cpp Floppy.cpp DMA.cpp FileSys.cpp Init.cpp Viewer.cpp OpNewDel.cpp String2.cpp Cursor.cpp Font.cpp ProcInfo.cpp Desktop.cpp PCI.cpp Renderer.cpp SurfMgr.cpp DebugConsole.cpp Serial.cpp SerialMouse.cpp BochsVideo.cpp VMwareVideo.cpp VGAVideo.cpp PCIList.cpp ATA.cpp CLGD5446Video.cpp FAT.cpp Cache.cpp Partition.cpp Reboot.cpp SAnim.cpp FillBenchmark.cpp CDFS.cpp S3Trio64Video.cpp Benchmark.cpp
+cl.exe -c -GR- -GS- -Gs999999999 -Gy -Ox -Oy -O2 -Oi- -fp:fast -Fo"%outdir%\\" Hello.cpp Loader.cpp Test.cpp i8042.cpp PS2Keyb.cpp PS2Mouse.cpp Floppy.cpp DMA.cpp FileSys.cpp Init.cpp Viewer.cpp OpNewDel.cpp String2.cpp Cursor.cpp Font.cpp ProcInfo.cpp Desktop.cpp PCI.cpp Renderer.cpp SurfMgr.cpp DebugConsole.cpp Serial.cpp SerialMouse.cpp BochsVideo.cpp VMwareVideo.cpp VGAVideo.cpp PCIList.cpp ATA.cpp CLGD5446Video.cpp FAT.cpp Cache.cpp Partition.cpp Reboot.cpp SAnim.cpp FillBenchmark.cpp CDFS.cpp S3Trio64Video.cpp Benchmark.cpp
+cd %outdir%
 link.exe %linkparams% -out:S3Trio64Video.bi_ S3Trio64Video.obj
 link.exe %linkparams% -out:CDFS.bi_ CDFS.obj
 link.exe %linkparams% -out:FillBenchmark.bi_ FillBenchmark.obj
@@ -28,7 +28,7 @@ link.exe %linkparams% -out:DMA.bi_ DMA.obj
 link.exe %linkparams% -out:FAT.bi_ FAT.obj
 link.exe %linkparams% -out:FileSys.bi_ FileSys.obj
 link.exe %linkparams% -out:Init.bi_ Init.obj
-link.exe %linkparams% -out:ProcInfo.bi_ ProcInfo.obj ulldiv.ob_
+link.exe %linkparams% -out:ProcInfo.bi_ ProcInfo.obj ..\ulldiv.obj
 link.exe %linkparams% -out:Desktop.bi_ Desktop.obj
 link.exe %linkparams% -out:PCI.bi_ PCI.obj
 link.exe %linkparams% -out:Renderer.bi_ Renderer.obj
@@ -43,52 +43,52 @@ link.exe %linkparams% -out:PCIList.bi_ PCIList.obj
 link.exe %linkparams% -out:ATA.bi_ ATA.obj
 link.exe %linkparams% -out:CLGD5446Video.bi_ CLGD5446Video.obj
 
-DriverStripper.exe i8042.bi_ "|0|" "(0)"
-DriverStripper.exe Serial.bi_ "|0|" "(0)"
+..\DriverStripper.exe i8042.bi_ "|0|" "(0)"
+..\DriverStripper.exe Serial.bi_ "|0|" "(0)"
 
-DriverStripper.exe PS2Keyb.bi_ "|1|" "(0)"
-DriverStripper.exe PS2Mouse.bi_ "|1|" "(0)"
-DriverStripper.exe SerialMouse.bi_ "|1|" "(0)"
+..\DriverStripper.exe PS2Keyb.bi_ "|1|" "(0)"
+..\DriverStripper.exe PS2Mouse.bi_ "|1|" "(0)"
+..\DriverStripper.exe SerialMouse.bi_ "|1|" "(0)"
 
-DriverStripper.exe Cursor.bi_ "|2|" "(1)"
+..\DriverStripper.exe Cursor.bi_ "|2|" "(1)"
 
-DriverStripper.exe DMA.bi_ "|3|" "(0)"
-DriverStripper.exe Floppy.bi_ "|3|" "(0)"
-DriverStripper.exe FileSys.bi_ "|3|" "(1)"
-DriverStripper.exe Partition.bi_ "|3|" "(0)"
-DriverStripper.exe Cache.bi_ "|3|" "(0)"
-DriverStripper.exe FAT.bi_ "|3|" "(0)"
-DriverStripper.exe CDFS.bi_ "|3|" "(0)"
-DriverStripper.exe PCI.bi_ "|3|" "(0)"
-DriverStripper.exe ATA.bi_ "|3|" "(0)"
+..\DriverStripper.exe DMA.bi_ "|3|" "(0)"
+..\DriverStripper.exe Floppy.bi_ "|3|" "(0)"
+..\DriverStripper.exe FileSys.bi_ "|3|" "(1)"
+..\DriverStripper.exe Partition.bi_ "|3|" "(0)"
+..\DriverStripper.exe Cache.bi_ "|3|" "(0)"
+..\DriverStripper.exe FAT.bi_ "|3|" "(0)"
+..\DriverStripper.exe CDFS.bi_ "|3|" "(0)"
+..\DriverStripper.exe PCI.bi_ "|3|" "(0)"
+..\DriverStripper.exe ATA.bi_ "|3|" "(0)"
 
-DriverStripper.exe Loader.bi_ "|4|" "(0)"
-DriverStripper.exe Init.bi_ "|4|" "(0)"
+..\DriverStripper.exe Loader.bi_ "|4|" "(0)"
+..\DriverStripper.exe Init.bi_ "|4|" "(0)"
 
-DriverStripper.exe Hello.bi_ "|6|" "(2)"
-DriverStripper.exe FillBenchmark.bi_ "|6|" "(2)"
-DriverStripper.exe Benchmark.bi_ "|6|" "(2)"
-DriverStripper.exe SAnim.bi_ "|6|" "(2)"
-DriverStripper.exe PCIList.bi_ "|6|" "(2)"
-DriverStripper.exe Viewer.bi_ "|6|" "(2)"
-DriverStripper.exe ProcInfo.bi_ "|1|" "(2)"
-DriverStripper.exe Reboot.bi_ "|6|" "(2)"
-DriverStripper.exe DebugConsole.bi_ "|6|" "(2)"
+..\DriverStripper.exe Hello.bi_ "|6|" "(2)"
+..\DriverStripper.exe FillBenchmark.bi_ "|6|" "(2)"
+..\DriverStripper.exe Benchmark.bi_ "|6|" "(2)"
+..\DriverStripper.exe SAnim.bi_ "|6|" "(2)"
+..\DriverStripper.exe PCIList.bi_ "|6|" "(2)"
+..\DriverStripper.exe Viewer.bi_ "|6|" "(2)"
+..\DriverStripper.exe ProcInfo.bi_ "|1|" "(2)"
+..\DriverStripper.exe Reboot.bi_ "|6|" "(2)"
+..\DriverStripper.exe DebugConsole.bi_ "|6|" "(2)"
 
-DriverStripper.exe Font.bi_ "|7|" "(0)"
-DriverStripper.exe Desktop.bi_ "|7|" "(1)"
-DriverStripper.exe S3Trio64Video.bi_ "|8|" "(0)"
-DriverStripper.exe BochsVideo.bi_ "|8|" "(0)"
-DriverStripper.exe CLGD5446Video.bi_ "|8|" "(0)"
-DriverStripper.exe VMwareVideo.bi_ "|8|" "(0)"
-DriverStripper.exe VGAVideo.bi_ "|8|" "(0)"
-DriverStripper.exe SurfMgr.bi_ "|8|" "(1)"
-DriverStripper.exe Renderer.bi_ "|9|" "(0)"
+..\DriverStripper.exe Font.bi_ "|7|" "(0)"
+..\DriverStripper.exe Desktop.bi_ "|7|" "(1)"
+..\DriverStripper.exe S3Trio64Video.bi_ "|8|" "(0)"
+..\DriverStripper.exe BochsVideo.bi_ "|8|" "(0)"
+..\DriverStripper.exe CLGD5446Video.bi_ "|8|" "(0)"
+..\DriverStripper.exe VMwareVideo.bi_ "|8|" "(0)"
+..\DriverStripper.exe VGAVideo.bi_ "|8|" "(0)"
+..\DriverStripper.exe SurfMgr.bi_ "|8|" "(1)"
+..\DriverStripper.exe Renderer.bi_ "|9|" "(0)"
 
-DriverStripper.exe Test.bi_ "|10|" "(2)"
+..\DriverStripper.exe Test.bi_ "|10|" "(2)"
 
-copy *.bin ..\Image\FloppyRoot\
-copy *.bin ..\Image\CDRoot\
+copy *.bin ..\..\Image\FloppyRoot\
+copy *.bin ..\..\Image\CDRoot\
 
-cd ..\Image\
+cd ..\..\Image\
 call Pack.bat
--- Kernel/Compile.bat
+++ Kernel/Compile.bat
@@ -1,8 +1,11 @@
-del Kernel.bin
-cl.exe -c -GS- -GL -GF -Ox -Oy -O2 -Zi main.cpp Kernel.cpp PTE.cpp PDE.cpp PhysMemManager.cpp GDT.cpp Global.cpp OpNewDel.cpp TSS.cpp Task.cpp IDT.cpp Thread.cpp IntManager.cpp
-link.exe -debug -nod -ltcg -opt:ref -fixed -base:0x20000 -entry:Entry -subsystem:windows -out:kernel.bi_ -filealign:4096 *.obj *.ob_
-KernelStripper.exe
-copy Kernel.bin ..\Image\FloppyRoot
-copy Kernel.bin ..\Image\CDRoot
-cd ..\Image\
+set outdir=Release
+mkdir %outdir%
+del %outdir%\* /Q
+cl.exe -c -GS- -GL -GF -Ox -Oy -O2 -Zi -Fo"%outdir%\\" /Fd"%outdir%\vc100.pdb" main.cpp Kernel.cpp PTE.cpp PDE.cpp PhysMemManager.cpp GDT.cpp Global.cpp OpNewDel.cpp TSS.cpp Task.cpp IDT.cpp Thread.cpp IntManager.cpp
+link.exe -debug -nod -ltcg -opt:ref -fixed -base:0x20000 -entry:Entry -subsystem:windows -out:%outdir%\kernel.bi_ -filealign:4096 %outdir%\*.obj *.obj
+cd %outdir%
+..\KernelStripper.exe
+copy Kernel.bin ..\..\Image\FloppyRoot
+copy Kernel.bin ..\..\Image\CDRoot
+cd ..\..\Image\
 call Pack.bat
