ab41ae9ff552fb9da32cbe5298fd5d8cb24bbe7b
Vort <vvort@yandex.ru>
2013-03-05 14:18:10 +0200

    Поддержка загрузки с пятидюймовых дискет

--- Image/FlpBootSect.asm
+++ Image/FlpBootSect.asm
@@ -8,16 +8,41 @@ EP:
    nop
 
 ; -----------------------------------------------
+;  disk_size = 360
+   disk_size = 1440
+
+   if disk_size = 1440
+     sectPerCluster    = 0x01
+     numRootDirEntries = 0x00e0
+     numSectors        = 0x0b40
+     mediaType	       = 0xf0
+     numFATsectors     = 0x0009
+     sectorsPerTrack   = 0x0012
+     rootDirSectCount  = 14
+     rootDirStartSect  = 19
+   else if disk_size = 360
+     sectPerCluster    = 0x02
+     numRootDirEntries = 0x0070
+     numSectors        = 0x02d0
+     mediaType	       = 0xfd
+     numFATsectors     = 0x0002
+     sectorsPerTrack   = 0x0009
+     rootDirSectCount  = 7
+     rootDirStartSect  = 5
+   end if
+
+
+; -----------------------------------------------
    OEMname	       db 'VORT_OS '
    bytesPerSector      dw 0x0200
-   sectPerCluster      db 0x01
+		       db sectPerCluster
    reservedSectors     dw 0x0001
    numFAT	       db 0x02
-   numRootDirEntries   dw 0x00e0
-   numSectors	       dw 0x0b40
-   mediaType	       db 0xf0
-   numFATsectors       dw 0x0009
-   sectorsPerTrack     dw 0x0012
+		       dw numRootDirEntries
+		       dw numSectors
+		       db mediaType
+		       dw numFATsectors
+		       dw sectorsPerTrack
    numHeads	       dw 0x0002
    numHiddenSectors    dd 0x00000000
    numSectorsHuge      dd 0x00000000
@@ -45,9 +70,9 @@ Start:
    ; 0xAC00 - 0xF000  Second Stage Boot
 
 
-   mov	 cx, 9	     ; FAT Sectors Count
-   mov	 ax, 1	     ; FAT Start Sector
-   mov	 bx, 0x7E0   ; FAT Start Segment
+   mov	 cx, numFATsectors     ; FAT Sectors Count
+   mov	 ax, 1		       ; FAT Start Sector
+   mov	 bx, 0x7E0	       ; FAT Start Segment
 
 FATReadLoop:
    push  bx
@@ -58,10 +83,11 @@ FATReadLoop:
    add	 bx, 0x20    ; Next Segment
    loop  FATReadLoop
 
-
-   mov	 cx, 14      ; Root Sectors Count
-   mov	 ax, 19      ; Root Start Sector
-   mov	 bx, 0x900   ; Root Start Segment
+   ; rootDirSectCount = numRootDirEntries / 16
+   mov	 cx, rootDirSectCount  ; Root Sectors Count
+   ; rootDirStartSect = 1 + numFATsectors * numFAT
+   mov	 ax, rootDirStartSect  ; Root Start Sector
+   mov	 bx, 0x900	       ; Root Start Segment
 
 RootReadLoop:
    push  bx
@@ -107,11 +133,23 @@ FATLoop:
    jae	 FATLoopExit  ; End Of Chain
 
    mov	 bx, ax
-   add	 bx, 0x1F     ; Cluster To Sector
+   sub	 bx, 2
+   if sectPerCluster = 2
+     shl   bx, 1
+   end if
+   add	 bx, rootDirSectCount + rootDirStartSect
 
    push  dx	      ; Segment
    push  bx	      ; Sector
    call  ReadSector
+
+   if sectPerCluster = 2
+     inc   bx
+     add   dx, 0x20
+     push  dx		; Segment
+     push  bx		; Sector
+     call  ReadSector
+   end if
 
    push  ax
    call  GetNextCluster
@@ -168,7 +206,7 @@ FindFile:
    pop	 ds	      ; ds = 0x0900
 
    xor	 si, si
-   mov	 dx, 0x00E0   ; Root Entries Count
+   mov	 dx, numRootDirEntries
 
 NextRootEntry:
    push  si
@@ -209,7 +247,7 @@ ReadSector:
 
    mov	 di, 0x03    ; Retry Count
 
-   mov	 si, 0x12    ; Sectors Per Track
+   mov	 si, sectorsPerTrack
    xor	 dx, dx
    div	 si
 
--- Image/MakeImage.bat
+++ Image/MakeImage.bat
@@ -1,22 +1,28 @@
 @echo off
 
-set ImageType=cdrom
+::set FloppySize=360
+set FloppySize=1440
 ::set ImageType=floppy
+set ImageType=cdrom
 
 echo Selected configuration: %ImageType%
+if "%ImageType%"=="floppy" echo Floppy size: %FloppySize%k
 
 if exist VortOS.iso del VortOS.iso
 if exist VortOS.ima del VortOS.ima
 copy OpenSans.vfnt ImageFiles\OpenSans.vfnt > nul
-if "%ImageType%"=="cdrom" goto cdrom
+if "%ImageType%"=="floppy" (
   if exist ImageFiles\CDBoot.bin del ImageFiles\CDBoot.bin
   copy FlpBoot.bin ImageFiles\FlpBoot.bin > nul
-  bfi -f=VortOS.ima -l=Vort_OS -b=FlpBootSect.bin ImageFiles
+  if "%FloppySize%"=="1440" (
+    bfi -t=6 -f=VortOS.ima -l=Vort_OS -b=FlpBootSect1440.bin ImageFiles
+  ) else (
+    bfi -t=3 -f=VortOS.ima -l=Vort_OS -b=FlpBootSect360.bin ImageFiles
+  )
   if not exist VortOS.ima exit /B -1
-goto end
-:cdrom
+) else (
   if exist ImageFiles\FlpBoot.bin del ImageFiles\FlpBoot.bin
   copy CDBoot.bin ImageFiles\CDBoot.bin > nul
   mkisofs.exe -b CDBoot.bin -no-emul-boot -boot-load-size 4 -o VortOS.iso -iso-level 4 ImageFiles
   if not exist VortOS.iso exit /B -1
-:end
+)
